0e507601b67212f0a439c2b1d4e6b74fd8b2e10e
Nia Laureano <nialaureano@icloud.com>
2019-08-12 04:56:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase()===string){
     return "I can\'t hear you!"
-  } else if (string.toUpperCase()===string)
+  } else if (string.toUpperCase()===string) {
+    return "YES INDEED!"
+  } else if (string.toLowerCase()==='I love you, Grandma.'){
+    return "I love you, too."
   }
+  return string.toLowerCase()
 }
  
